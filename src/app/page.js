266d419ef2bffcb754a3d3c26b411aec0849d281
CatalinaScheleff <catascheleff@gmail.com>
2024-04-28 21:26:15 -0400

    Inicio y Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,28 @@
-import Hero1 from "@/components/Hero1";
+
 
 
 export default function Inicio() {
   return (
     <main>
-    <Hero1/>
+     <div
+        className="hero h-[550px] bg-cover bg-center"
+        style={{ backgroundImage: `url(/inicio.png)` }}
+      >
+       
+      </div>
+      <div className="text-center m-5">
+        <div>
+          <h1 className="text-[60px] font-bold">Trashumante Audiovisual</h1>
+        </div>
+       
+        <div className="m-4">
+          <p>
+            From the small stuff to the big picture, organizes the work so teams
+            know what to do, why it matters, and how to get it done.
+          </p>
+      
+        </div>
+      </div>
     </main>
   );
 }
