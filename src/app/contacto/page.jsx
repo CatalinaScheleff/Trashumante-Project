697ff397a78a0eaaf93bfb86c98ecb3cdc0b612b
CatalinaScheleff <catascheleff@gmail.com>
2024-05-03 22:40:41 -0400

    EmailJS

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -1,4 +1,10 @@
+'use client'
+import React, { useClient } from 'next';
+import ContactForm from '@/components/ContactForm';
+
+
 const Contacto = () => {
+
   return (
     <main className="pb-16">
       <div className="grid-cols-2 grid p-10">
@@ -17,88 +23,8 @@ const Contacto = () => {
           <p className="pb-3">Facebook</p>
         </div>
 
-{/* col s */}
-        <div className="">
-          <label className="form-control w-full max-w-ls mb-5">
-            <div className="label">
-              <span className="label-text">Nombre</span>
-            </div>
-            <input
-              type="text"
-              placeholder="Escriba su nombre aquí"
-              className="bg-gray-100 input w-full max-w-xl"
-            />
-          </label>
-          <label className="form-control w-full max-w-ls mb-5">
-            <div className="label">
-              <span className="label-text">Email</span>
-            </div>
-            <input
-              type="text"
-              placeholder="Escriba su email aquí"
-              className="bg-gray-100 input w-full max-w-xl"
-            />
-          </label>
-          <div className="pb-4">
-          <div className="label">
-            <span className="label-text">
-              ¿En qué servicios estás interesado?
-            </span>
-          </div>
-          <div
-            tabIndex={0}
-            className="collapse collapse-arrow max-w-xl bg-gray-100"
-          >
-            <input type="checkbox" className="peer" />
-            <div className="collapse-title text-ms text-gray-400 ">
-              Seleccione servicios
-            </div>
-            <div className="collapse-content">
-              <div className="form-control">
-                <label className="label cursor-pointer">
-                  <span className="label-text">Fotografía</span>
-                  <input
-                    type="checkbox"
-                    className="w-5 h-5 accent-violet-600"
-                  />
-                </label>
-              </div>
-              <div className="form-control">
-                <label className="label cursor-pointer">
-                  <span className="label-text">Videoclip</span>
-                  <input
-                    type="checkbox"
-                    className="w-5 h-5 accent-violet-600"
-                  />
-                </label>
-              </div>
-              <div className="form-control">
-                <label className="label cursor-pointer">
-                  <span className="label-text">Producción de eventos</span>
-                  <input
-                    type="checkbox"
-                    className="w-5 h-5 accent-violet-600"
-                  />
-                </label>
-              </div>
-            </div>
-          </div>
-          </div>
-          <div className="pb-5">
-          <div className="label">
-            <span className="label-text">Mensaje</span>
-          </div>
-          <textarea
-            id="message"
-            rows="4"
-            className="block p-2.5 w-full max-w-xl text-sm bg-gray-100 rounded-lg focus:ring-violet-600 focus:border-violet-600"
-            placeholder="Escribe tu mensaje aquí..."
-          ></textarea>
-          </div>
-        <div className="card-actions w-full max-w-xl justify-end">
-      <button className="btn bg-violet-600 text-white hover:text-violet-600 border-solid border-2 border-violet-600">Enviar</button>
-    </div>
-        </div>
+{/* col 2 */}
+        <ContactForm/>
       </div>
     </main>
   );
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,113 @@
+import React, { useRef, useClient } from 'react';
+import emailjs from '@emailjs/browser';
+
+const ContactForm = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+        .sendForm('service_64n06xt', 'template_xlwnhzl', form.current, {
+          publicKey: 'TQVeMt3movr_IcdUN',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    };
+
+
+    return ( 
+        <form ref={form} onSubmit={sendEmail} className="">
+          <label className="form-control w-full max-w-ls mb-5">
+            <div className="label">
+              <span className="label-text">Nombre</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Escriba su nombre aquí"
+              className="bg-gray-100 input w-full max-w-xl"
+              name='nombre'
+            />
+          </label>
+          <label className="form-control w-full max-w-ls mb-5">
+            <div className="label">
+              <span className="label-text">Email</span>
+            </div>
+            <input
+              type="email"
+              placeholder="Escriba su email aquí"
+              className="bg-gray-100 input w-full max-w-xl"
+              name='email'
+            />
+          </label>
+          <div className="pb-4">
+          <div className="label">
+            <span className="label-text">
+              ¿En qué servicios estás interesado?
+            </span>
+          </div>
+          <div
+            tabIndex={0}
+            className="collapse collapse-arrow max-w-xl bg-gray-100"
+          >
+            <input type="checkbox" className="peer" />
+            <div className="collapse-title text-ms text-gray-400 ">
+              Seleccione servicios
+            </div>
+            <div className="collapse-content">
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Fotografía</span>
+                  <input
+                    type="checkbox"
+                    className="w-5 h-5 accent-violet-600"
+                  />
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Videoclip</span>
+                  <input
+                    type="checkbox"
+                    className="w-5 h-5 accent-violet-600"
+                  />
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text">Producción de eventos</span>
+                  <input
+                    type="checkbox"
+                    className="w-5 h-5 accent-violet-600"
+                  />
+                </label>
+              </div>
+            </div>
+          </div>
+          </div>
+          <div className="pb-5">
+          <div className="label">
+            <span className="label-text">Mensaje</span>
+          </div>
+          <textarea
+            id="message"
+            name='mensaje'
+            rows="4"
+            className="block p-2.5 w-full max-w-xl text-sm bg-gray-100 rounded-lg focus:ring-violet-600 focus:border-violet-600"
+            placeholder="Escribe tu mensaje aquí..."
+          ></textarea>
+          </div>
+        <div className="card-actions w-full max-w-xl justify-end">
+      <button type='sumbit' value="Send" className="btn bg-violet-600 text-white hover:text-violet-600 border-solid border-2 border-violet-600">Enviar</button>
+    </div>
+        </form>
+     );
+}
+ 
+export default ContactForm;
