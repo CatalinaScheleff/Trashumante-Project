2b8171807c91cb287c85f64194c5bf03bcd6cb6f
CatalinaScheleff <catascheleff@gmail.com>
2024-05-04 20:03:17 -0400

    validaciones formulario de contacto

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,11 +1,21 @@
-import React, { useRef, useClient } from 'react';
+import React, { useRef, useClient, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
     const form = useRef();
+    const [showValidation, setShowValidation] = useState(false);
 
     const sendEmail = (e) => {
       e.preventDefault();
+
+      const nombre = form.current['nombre'].value;
+      const email = form.current['email'].value;
+      const mensaje = form.current['mensaje'].value;
+      if (!nombre || !email || !mensaje) {
+        setShowValidation(true); // Activar mensajes de validación
+        alert("Por favor complete todos los campos");
+        return;
+      }
   
       emailjs
         .sendForm('service_64n06xt', 'template_xlwnhzl', form.current, {
@@ -14,11 +24,16 @@ const ContactForm = () => {
         .then(
           () => {
             console.log('SUCCESS!');
+            alert("Mensaje enviado con exito")
           },
           (error) => {
             console.log('FAILED...', error.text);
           },
         );
+    };
+
+    const handleInputChange = (e) => {
+      setShowValidation(false);
     };
 
 
@@ -33,7 +48,9 @@ const ContactForm = () => {
               placeholder="Escriba su nombre aquí"
               className="bg-gray-100 input w-full max-w-xl"
               name='nombre'
+              onChange={handleInputChange}
             />
+            {showValidation && form.current['nombre'].value.length === 0 && <span className="text-red-600 text-sm">Este campo es requerido</span>}
           </label>
           <label className="form-control w-full max-w-ls mb-5">
             <div className="label">
@@ -44,7 +61,9 @@ const ContactForm = () => {
               placeholder="Escriba su email aquí"
               className="bg-gray-100 input w-full max-w-xl"
               name='email'
+              onChange={handleInputChange}
             />
+            {showValidation && form.current['email'].value.length === 0 && <span className="text-red-600 text-sm">Este campo es requerido</span>}
           </label>
           <div className="pb-4">
           <div className="label">
@@ -92,7 +111,9 @@ const ContactForm = () => {
             rows="4"
             className="block p-2.5 w-full max-w-xl text-sm bg-gray-100 rounded-lg focus:ring-violet-600 focus:border-violet-600"
             placeholder="Escribe tu mensaje aquí..."
+            onChange={handleInputChange}
           ></textarea>
+          {showValidation && form.current['mensaje'].value.length === 0 && <span className="text-red-600 text-sm">Este campo es requerido</span>}
           </div>
         <div className="card-actions w-full max-w-xl justify-end">
       <button type='sumbit' value="Send" className="btn bg-violet-600 text-white hover:text-violet-600 border-solid border-2 border-violet-600">Enviar</button>
