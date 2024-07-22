2b8125287cb9a0622a81238897f6153fd32b2de1
CatalinaScheleff <catascheleff@gmail.com>
2024-07-22 17:08:31 -0400

    toastify

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,13 +1,14 @@
-import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
 
 const ContactForm = () => {
   const form = useRef();
   const [showValidation, setShowValidation] = useState(false);
   const [formData, setFormData] = useState({
-    nombre: '',
-    email: '',
-    mensaje: '',
+    nombre: "",
+    email: "",
+    mensaje: "",
     fotografia: false,
     videoclip: false,
     produccionDeEventos: false,
@@ -24,16 +25,26 @@ const ContactForm = () => {
     }
 
     emailjs
-      .sendForm('service_64n06xt', 'template_xlwnhzl', form.current, 'TQVeMt3movr_IcdUN')
+      .sendForm(
+        "service_64n06xt",
+        "template_xlwnhzl",
+        form.current,
+        "TQVeMt3movr_IcdUN"
+      )
       .then(
         () => {
-          console.log('SUCCESS!');
-          alert("Mensaje enviado con éxito");
+          console.log("SUCCESS!");
+          toast.success("Mensaje enviado exitosamente!", {
+            position: "top-center",
+            autoClose: 5000,
+            closeOnClick: true,
+            
+          });
           // Clear form fields
           setFormData({
-            nombre: '',
-            email: '',
-            mensaje: '',
+            nombre: "",
+            email: "",
+            mensaje: "",
             fotografia: false,
             videoclip: false,
             produccionDeEventos: false,
@@ -42,7 +53,7 @@ const ContactForm = () => {
           setDropdownOpen(false);
         },
         (error) => {
-          console.log('FAILED...', error.text);
+          console.log("FAILED...", error.text);
         }
       );
   };
@@ -52,7 +63,7 @@ const ContactForm = () => {
     setShowValidation(false);
     setFormData((prevData) => ({
       ...prevData,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === "checkbox" ? checked : value,
     }));
   };
 
@@ -74,7 +85,9 @@ const ContactForm = () => {
           value={formData.nombre}
           onChange={handleInputChange}
         />
-        {showValidation && formData.nombre.length === 0 && <span className="text-red-600 text-sm">Este campo es requerido</span>}
+        {showValidation && formData.nombre.length === 0 && (
+          <span className="text-red-600 text-sm">Este campo es requerido</span>
+        )}
       </label>
       <label className="form-control w-full max-w-ls mb-5">
         <div className="label">
@@ -88,15 +101,26 @@ const ContactForm = () => {
           value={formData.email}
           onChange={handleInputChange}
         />
-        {showValidation && formData.email.length === 0 && <span className="text-red-600 text-sm">Este campo es requerido</span>}
+        {showValidation && formData.email.length === 0 && (
+          <span className="text-red-600 text-sm">Este campo es requerido</span>
+        )}
       </label>
       <div className="pb-4">
         <div className="label">
-          <span className="label-text">¿En qué servicios estás interesado?</span>
+          <span className="label-text">
+            ¿En qué servicios estás interesado?
+          </span>
         </div>
-        <div tabIndex={0} className={`collapse collapse-arrow max-w-xl bg-gray-100 ${dropdownOpen ? 'collapse-open' : 'collapse-close'}`}>
+        <div
+          tabIndex={0}
+          className={`collapse collapse-arrow max-w-xl bg-gray-100 ${
+            dropdownOpen ? "collapse-open" : "collapse-close"
+          }`}
+        >
           <input type="checkbox" className="peer" onChange={toggleDropdown} />
-          <div className="collapse-title text-ms text-gray-400">Seleccione servicios</div>
+          <div className="collapse-title text-ms text-gray-400">
+            Seleccione servicios
+          </div>
           <div className="collapse-content">
             <div className="form-control">
               <label className="label cursor-pointer">
@@ -107,7 +131,7 @@ const ContactForm = () => {
                   name="fotografia"
                   checked={formData.fotografia}
                   onChange={handleInputChange}
-                  value={'Fotografía'}
+                  value={"Fotografía"}
                 />
               </label>
             </div>
@@ -120,7 +144,7 @@ const ContactForm = () => {
                   name="videoclip"
                   checked={formData.videoclip}
                   onChange={handleInputChange}
-                  value={'Videoclip'}
+                  value={"Videoclip"}
                 />
               </label>
             </div>
@@ -133,7 +157,7 @@ const ContactForm = () => {
                   name="produccionDeEventos"
                   checked={formData.produccionDeEventos}
                   onChange={handleInputChange}
-                  value={'Producción de eventos'}
+                  value={"Producción de eventos"}
                 />
               </label>
             </div>
@@ -153,13 +177,21 @@ const ContactForm = () => {
           value={formData.mensaje}
           onChange={handleInputChange}
         ></textarea>
-        {showValidation && formData.mensaje.length === 0 && <span className="text-red-600 text-sm">Este campo es requerido</span>}
+        {showValidation && formData.mensaje.length === 0 && (
+          <span className="text-red-600 text-sm">Este campo es requerido</span>
+        )}
       </div>
       <div className="card-actions w-full max-w-xl justify-end">
-        <button type="submit" value="Send" className="btn bg-violet-600 text-white hover:text-violet-600 border-solid border-2 border-violet-600">Enviar</button>
+        <button
+          type="submit"
+          value="Send"
+          className="btn bg-violet-600 text-white hover:text-violet-600 border-solid border-2 border-violet-600"
+        >
+          Enviar
+        </button>
       </div>
     </form>
   );
-}
+};
 
 export default ContactForm;
