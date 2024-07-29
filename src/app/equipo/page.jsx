fc0c50b85a6b0851189a211194500e367d5779d9
CatalinaScheleff <catascheleff@gmail.com>
2024-07-29 19:20:00 -0400

    equipo responsivo

--- src/app/equipo/page.jsx
+++ src/app/equipo/page.jsx
@@ -6,13 +6,13 @@ import {CardEquipo} from "@/components/Cards";
 const Equipo = () => {
  console.log(info)
   return (
-    <main className="mx-24">
+    <main className="lg:mx-24 pb-20 md:pb-4">
       <div className="px-5 text-center mt-16">
         <h1 className="text-5xl font-bold mb-16">Equipo</h1>
       </div>
 
         
-          <div className="grid gap-8 grid-cols-4 place-items-center">     
+          <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center">     
 
           {
                 info?.equipo.map((e) => {
@@ -25,82 +25,6 @@ const Equipo = () => {
                     )
                 })
             }
-
-
-
-
-{/* <div className="card card-compact w-64 bg-base-100 shadow-xl ">
-  <figure className="h-64 w-64 bg-slate-600">
-    <Image src="/inicio.png" alt="Shoes" width={384} height={384} className="w-full h-full object-cover"/>
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Bayron Espinoza</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-        <Link href={"/equipo/bayronespinoza"}>
-      <button className="btn bg-violet-600 text-white hover:text-violet-600 border-solid border-2 border-violet-600">
-        Más información
-      </button>
-        </Link>
-    </div>
-  </div>
-</div>
-
-<div className="card card-compact w-64 bg-base-100 shadow-xl ">
-  <figure className="h-64 w-64 bg-slate-600">
-    <Image src="/inicio.png" alt="Shoes" width={384} height={384} className="w-full h-full object-cover"/>
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Bayron Espinoza</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-        <Link href={"/equipo/bayronespinoza"}>
-      <button className="btn bg-violet-600 text-white hover:text-violet-600 border-solid border-2 border-violet-600">
-        Más información
-      </button>
-        </Link>
-    </div>
-  </div>
-</div>
-
-<div className="card card-compact w-64 bg-base-100 shadow-xl ">
-  <figure className="h-64 w-64 bg-slate-600">
-    <Image src="/inicio.png" alt="Shoes" width={384} height={384} className="w-full h-full object-cover"/>
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Bayron Espinoza</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-        <Link href={"/equipo/bayronespinoza"}>
-      <button className="btn bg-violet-600 text-white hover:text-violet-600 border-solid border-2 border-violet-600">
-        Más información
-      </button>
-        </Link>
-    </div>
-  </div>
-</div>
-
-<div className="card card-compact w-64 bg-base-100 shadow-xl ">
-  <figure className="h-64 w-64 bg-slate-600">
-    <Image src="/inicio.png" alt="Shoes" width={384} height={384} className="w-full h-full object-cover"/>
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Bayron Espinoza</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-        <Link href={"/equipo/bayronespinoza"}>
-      <button className="btn bg-violet-600 text-white hover:text-violet-600 border-solid border-2 border-violet-600">
-        Más información
-      </button>
-        </Link>
-    </div>
-  </div>
-</div> */}
-
-
-
-            
-
             </div>
 
             <div className="h-28"></div>
