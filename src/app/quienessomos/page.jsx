38558ad199e479d0f7f4dacd3b41caf88429e660
CatalinaScheleff <catascheleff@gmail.com>
2024-07-29 19:12:10 -0400

    quienes somos responsivo

--- src/app/quienessomos/page.jsx
+++ src/app/quienessomos/page.jsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
+
 const QuienesSomos = () => {
   return (
-    <main className="my-16">
-      <div className="m-20">
-        <div className=" grid grid-cols-2 col-auto">
+    <main className="pb-40">
+      <div className="mx-8 mt-6 lg:mt-24">
+        <div className=" grid md:grid-cols-2 col-auto">
 
-          <div className="col px-5">
-            <h1 className="text-5xl font-bold mb-10">¿Quienes Somos?</h1>
+          <div className="col lg:px-5">
+            <h1 className="text-3xl md:text-[4xl] lg:text-5xl font-bold mb-10">¿Quienes Somos?</h1>
             <p className="my-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus fringilla dui amet faucibus nam. Erat id laoreet posuere etiam morbi.
             </p>
@@ -20,11 +22,27 @@ const QuienesSomos = () => {
             </p>
           </div>
 
-          <div className="col grid grid-cols-2 grid-rows-6 h-[560px]">
-            <div className="col-span-1 row-span-5 bg-slate-600"> imagen</div>
-            <div className="col-span-1 row-span-1"> col2</div>
-            <div className="col-span-1 row-span-5 bg-slate-600"> imagen</div>
-            <div className="col-span-1 row-span-1"> col1</div>
+          <div className="col grid lg:grid-cols-2 lg:grid-rows-6 h-[1000px] lg:h-[560px] md:mx-4 lg:mx-12 mt-8">
+            <div className="col-span-1 row-span-5 bg-slate-600"> 
+            <Image
+              src={'/1.jpg'}
+              alt=""
+              width={650}
+              height={560}
+              className="w-full h-full object-cover"
+            />
+            </div>
+            <div className="col-span-1 row-span-1"></div>
+            <div className="col-span-1 row-span-5 bg-slate-600"> 
+            <Image
+              src={'/inicio.png'}
+              alt=""
+              width={650}
+              height={560}
+              className="w-full h-full object-cover"
+            />
+            </div>
+            <div className="col-span-1 row-span-1"></div>
             
           </div>
 
